206b956ec3104a0c294c633d1e212c5bfa0c001b
farhat-h <farhathaythem1@gmail.com>
2020-09-18 17:19:12 +0200

    v2

---
+++ precache-manifest.3d6245261a9abe9e292ac90fd4e11b36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "191f351a7b95651ba2c0aed1afe1be76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f2094bc10cb6ac5fba",
+    "url": "/static/css/main.23458222.chunk.css"
+  },
+  {
+    "revision": "5c24501f146e9b224166",
+    "url": "/static/js/2.1f8c163a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/static/js/2.1f8c163a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f2094bc10cb6ac5fba",
+    "url": "/static/js/main.da4655f1.chunk.js"
+  },
+  {
+    "revision": "55cf03ad92c385910fbc",
+    "url": "/static/js/runtime-main.e40405f4.js"
+  }
+]);
